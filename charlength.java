2392e90672d791c5534b40c98ccc8b4443ea0ea4
santhoshini <santhoshini3333@gmail.com>
2025-10-24 19:36:39 +0530

    length of the string

---
+++ charlength.java
@@ -0,0 +1,12 @@
+public class charlength {
+    public static void main(String[] args) {
+        String str="santhoshini";
+        String digits="";
+        String chars=" ";
+        for(int i=0;i<str.length();i++) {
+            char ch=str.charAt(i);
+        }    
+        System.out.println("the length of the string is:"+str.length());
+    }
+    
+}
